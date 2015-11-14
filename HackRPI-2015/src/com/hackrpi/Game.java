244a8695c915de34a031a113e9d013e268d80846
Charlie Schmitter <schmic6@rpi.edu>
2015-11-14 13:37:04 -0500

    Second commit, more changes.

--- HackRPI-2015/src/com/hackrpi/Game.java
+++ HackRPI-2015/src/com/hackrpi/Game.java
@@ -1,8 +1,19 @@
 package com.hackrpi;
 
-public class Game
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Game extends StateBasedGame
 {
+	public static int mainMenu = 0;
 	
+	public Game(String name)
+	{
+		super(name);
+		addState(new MainMenu(mainMenu));
+	}
 	
 	public static void main(String[] args)
 	{

af8e518b6258098d06bc9114cd78ce0fdd476b27
Nate Hilscher <thisisnotnate@gmail.com>
2015-11-14 16:51:30 -0500

    kappa

--- HackRPI-2015/src/com/hackrpi/Projectile.java
+++ HackRPI-2015/src/com/hackrpi/Projectile.java
@@ -2,17 +2,19 @@ package com.hackrpi;
 
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.SpriteSheet;
 
-public class Projectile 
+public class Projectile extends Entity
 {
 	private int damage, speed, width, length;
 	private Image image;
 	private SpriteSheet ss;
 	private Animation anim;
 	
-	public Projectile(int d, int s, int w, int l, Image im)
+	public Projectile(int d, int s, int w, int l, Image im, float x, float y) throws SlickException
 	{
+		super(x,y,99999,99999);
 		damage = d;
 		speed = s;
 		width = w;
@@ -20,8 +22,9 @@ public class Projectile
 		image = im;
 		ss = new SpriteSheet(image, 0,0); // Implement the location on subclass;
 		anim = new Animation(ss, 0); // implement with subclass
+		
 	}
 	
-	public int getDamage(){return damage;} // test
+	public int getDamage(){return damage;}
 	
 }

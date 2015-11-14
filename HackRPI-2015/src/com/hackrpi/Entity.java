7430ea3ded918c6e8aee63127082c37f05b88554
Nate Hilscher <thisisnotnate@gmail.com>
2015-11-14 17:16:18 -0500

    added collision

--- HackRPI-2015/src/com/hackrpi/Entity.java
+++ HackRPI-2015/src/com/hackrpi/Entity.java
@@ -1,5 +1,7 @@
 package com.hackrpi;
 
+import java.awt.Rectangle;
+
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
@@ -10,15 +12,44 @@ import org.newdawn.slick.state.StateBasedGame;
 public class Entity 
 {
 	static float posY, posX, speed;
-	static int health, healthMax, time; 
+	static int health, healthMax, time, width, height; 
 	
 	public Entity(){}
 	
-	public Entity(float x, float y, int h, int hMax) throws SlickException
+	public Entity(float x, float y, int h, int hMax, int w, int he) throws SlickException
 	{
 		posX = x;
 		posY = y;
 		health = h;
-		healthMax = hMax;		
+		healthMax = hMax;	
+		width = w;
+		height = he;
 	}
+	
+	public boolean collides(Entity x)
+	{
+		Rectangle r1 = new Rectangle((int)posX, (int)posY, this.getWidth(), this.getHeight());
+		Rectangle r2 = new Rectangle((int)x.getPosX(), (int)x.getPosY(), x.getWidth(), x.getHeight());
+		return r1.intersects(r2);
+	}
+
+	public static float getPosY() 
+	{
+		return posY;
+	}
+
+	public static float getPosX() 
+	{
+		return posX;
+	}
+
+	public static int getWidth() {
+		return width;
+	}
+
+	public static int getHeight() {
+		return height;
+	}
+	
+	
 }

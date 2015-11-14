c092357db475c685fea6865bd1a784b0c3bf8956
Nate Hilscher <thisisnotnate@gmail.com>
2015-11-14 16:51:59 -0500

    kappa

--- HackRPI-2015/src/com/hackrpi/Projectile.java
+++ HackRPI-2015/src/com/hackrpi/Projectile.java
@@ -14,7 +14,7 @@ public class Projectile extends Entity
 	
 	public Projectile(int d, int s, int w, int l, Image im, float x, float y) throws SlickException
 	{
-		super(x,y,99999,99999);
+		super(x,y,99999,99999); //push
 		damage = d;
 		speed = s;
 		width = w;

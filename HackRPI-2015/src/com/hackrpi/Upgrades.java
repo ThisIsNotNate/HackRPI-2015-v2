85c422870c3c888a16804d849082269e8b135487
Charlie Schmitter <schmic6@rpi.edu>
2015-11-14 22:58:12 -0500

    images

--- HackRPI-2015/src/com/hackrpi/Upgrades.java
+++ HackRPI-2015/src/com/hackrpi/Upgrades.java
@@ -1,32 +1,52 @@
 package com.hackrpi;
 
+import com.hackrpi.gui.Button;;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.Image;
 
 public class Upgrades extends BasicGameState
 {
-	public Upgrades(int state){}
+	Image background;
+	Button cadmiumYellowUpgrade;
+	Button alizarinCrimsonUpgrade;
+	
+	public Upgrades(int state)
+	{
+		
+	}
 	
 	public void init(GameContainer container, StateBasedGame sbg) throws SlickException 
 	{
-
+		background = new Image("res/upgradeMenu.png");
+		
+		alizarinCrimsonUpgrade = new Button("res/alizarinCrimsonUpgrade.png", 384, 155, 512, 120);
+		cadmiumYellowUpgrade = new Button("res/cadmiumYellowUpgrade.png", 384, 465, 512, 120);
 	}
 
-	@Override
 	public void render(GameContainer container, StateBasedGame sbg, Graphics g) throws SlickException
 	{
-		Input input = container.getInput();
-		
+		g.drawImage(background, 0, 0);
+		alizarinCrimsonUpgrade.render(g);
+		cadmiumYellowUpgrade.render(g);
 	}
 
-	@Override
 	public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException 
 	{
-
+		Input input = container.getInput(); 
+		
+		if(alizarinCrimsonUpgrade.isClicked(input))
+		{
+			
+		}
+		if(cadmiumYellowUpgrade.isClicked(input))
+		{
+			
+		}
 	}
 	
 	@Override

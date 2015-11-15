177c5d55d8f88f8e727c2a66a3df1124cb1a9e37
Nate Hilscher <thisisnotnate@gmail.com>
2015-11-15 05:03:55 -0500

    extra button

--- HackRPI-2015/src/com/hackrpi/End.java
+++ HackRPI-2015/src/com/hackrpi/End.java
@@ -2,30 +2,36 @@ package com.hackrpi;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import com.hackrpi.gui.Button;
+
 public class End extends BasicGameState
 {
-	
+	Button exitButton;
 	public End(int state){}
 	
 	public void init(GameContainer container, StateBasedGame sbg) throws SlickException 
 	{
-
+		exitButton = new Button("res/exitButton.png", 480, 480, 320, 90);
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame sbg, Graphics g) throws SlickException
 	{
-		
+		exitButton.render(g);
 	}
 
 	@Override
 	public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException 
 	{
-
+		Input input = container.getInput();
+		if(exitButton.isClicked(input))
+			sbg.enterState(Game.MAIN_MENU);
+			
 	}
 	
 	@Override

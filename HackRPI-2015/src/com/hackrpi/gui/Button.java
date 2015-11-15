0a98948b3ff04f71735269b7553f8c3ca80d9a39
Talha215 <bmw215t@aim.com>
2015-11-15 04:29:21 -0500

    button sounds

--- HackRPI-2015/src/com/hackrpi/gui/Button.java
+++ HackRPI-2015/src/com/hackrpi/gui/Button.java
@@ -2,7 +2,9 @@ package com.hackrpi.gui;
 
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
+import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.Graphics;
 
@@ -11,6 +13,7 @@ public class Button
 	private Image image;
 	private int x, y;
 	private int width, height;
+	private Sound menuSound, confirmSound;
 	
 	
 	public Button(String path, int x, int y, int width, int height) throws SlickException
@@ -20,13 +23,18 @@ public class Button
 		this.y = y;
 		this.width = width;
 		this.height = height;
+		menuSound = new Sound("res/menuButton.ogg");
+		confirmSound = new Sound("res/accept.ogg");
 	}
 	
 	public boolean isMouseWithinBounds(Input input)
 	{
 		if(input.getMouseX() >= x && input.getMouseX() <= x + width
 		&& input.getMouseY() >= y && input.getMouseY() <= y + height)
+		{
+			menuSound.play();
 			return true;
+		}
 		else
 			return false;
 	}
@@ -34,7 +42,10 @@ public class Button
 	public boolean isClicked(Input input)
 	{
 		if(isMouseWithinBounds(input) && input.isMousePressed(Input.MOUSE_LEFT_BUTTON))
+		{
+			confirmSound.play();
 			return true;
+		}
 		else
 			return false;
 	}

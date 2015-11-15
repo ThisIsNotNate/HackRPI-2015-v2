3dc9238b463472603128a94ffe594f3073839f78
Nate Hilscher <thisisnotnate@gmail.com>
2015-11-15 05:30:43 -0500

    End

--- HackRPI-2015/src/com/hackrpi/End.java
+++ HackRPI-2015/src/com/hackrpi/End.java
@@ -4,6 +4,7 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -12,9 +13,11 @@ import com.hackrpi.gui.Button;
 public class End extends BasicGameState
 {
 	Button exitButton;
+	Sound spooky;
+	private boolean played = false;
 	public End(int state){}
 	
-	public void init(GameContainer container, StateBasedGame sbg) throws SlickException 
+	public void init(GameContainer container, StateBasedGame sbg) throws SlickException
 	{
 		exitButton = new Button("res/exitButton.png", 480, 480, 320, 90);
 	}
@@ -28,6 +31,19 @@ public class End extends BasicGameState
 	@Override
 	public void update(GameContainer container, StateBasedGame sbg, int delta) throws SlickException 
 	{
+		
+		if(!played)
+		{
+			try {
+				Thread.sleep(1500);
+				spooky = new Sound("res/BeatTheDevil.wav");
+				spooky.play();
+				played = true;
+
+			} catch (InterruptedException e) {
+
+			}
+		}
 		Input input = container.getInput();
 		if(exitButton.isClicked(input))
 			sbg.enterState(Game.MAIN_MENU);

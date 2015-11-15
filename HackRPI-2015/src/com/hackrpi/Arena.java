338992bd0d92ac60650b0a4ec39664e9a38eacc6
Nate Hilscher <thisisnotnate@gmail.com>
2015-11-15 11:55:00 -0500

    red BAAAAAAAAAAAALLLLL

--- HackRPI-2015/src/com/hackrpi/Arena.java
+++ HackRPI-2015/src/com/hackrpi/Arena.java
@@ -7,6 +7,7 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -124,6 +125,7 @@ public class Arena extends BasicGameState
 				   	enemies.add(new Tree());
 				   	enemies.add(new Tree());
 				   	enemies.add(new Tree());
+				   	enemies.add(new Cloud());
 				   break;
 			
 			default:
